2b7a69e487e9b8c313b010a280bc5f61fee1b0f7
AlexisAndre34 <alexis.andrepro34@gmail.com>
2020-03-31 18:29:07 +0200

    add a case

--- src/app/add-cases/add-cases.component.ts
+++ src/app/add-cases/add-cases.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-add-cases',
@@ -6,10 +18,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-cases.component.css']
 })
 export class AddCasesComponent implements OnInit {
+  casesForm: FormGroup;
+  name = '';
+  gender = '';
+  age: number = null;
+  address = '';
+  city = '';
+  country = '';
+  status = '';
+  statusList = ['Positive', 'Dead', 'Recovered'];
+  genderList = ['Male', 'Female'];
+  isLoadingResults = false;
+  matcher = new MyErrorStateMatcher();
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private api: ApiService,
+    private formBuilder: FormBuilder,
+  ) { }
 
-  ngOnInit() {
+  onFormSubmit() {
+    this.isLoadingResults = true;
+    this.api.addCases(this.casesForm.value)
+      .subscribe((res: any) => {
+        const id = res._id
+        this.isLoadingResults = false;
+        this.router.navigate(['/cases-details', id]);
+      }, (err: any) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      });
+  }
+
+  ngOnInit(): void {
+    this.casesForm = this.formBuilder.group({
+      name: [null, Validators.required],
+      gender : [null, Validators.required],
+      age : [null, Validators.required],
+      address : [null, Validators.required],
+      city : [null, Validators.required],
+      country : [null, Validators.required],
+      status : [null, Validators.required]
+    });
   }
 
 }
